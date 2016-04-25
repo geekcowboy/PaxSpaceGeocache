cb55cfbe524289780ce8f5141e71ad8fd631f829
Mike <mike@greenshoegarage.com>
2016-04-25 16:02:46 -0400

    switch from Solenoid to Servo
    Lock mechanism will now be accomplished with a servo

--- Software/PaxSpaceGeocache/PaxSpaceGeocache.ino
+++ Software/PaxSpaceGeocache/PaxSpaceGeocache.ino
@@ -1,17 +1,28 @@
+//**********************************
+//
+//
+//**********************************
+
+#include <Servo.h>
+
 #define HallSensor1 0
 #define HallSensor2 1
 #define HallSensor3 2
 #define HallSensorLock 3
 
-#define Solenoid1 8
-#define Solenoid2 9
+#define Servo1_pin 9
+#define Servo2_pin 10
 
 #define LED1 11
 #define LED2 12
 #define LED3 13
 
 #define HallOnOff 100
-#define troubleshootDelay 3000
+#define TroubleshootDelay 3000
+#define Servo1_openPos 0
+#define Servo2_openPos 90
+#define Servo1_closePos 90
+#define Servo2_closePos 0
 
 int Status_HallSensor1 = 0;
 int Status_HallSensor2 = 0;
@@ -27,22 +38,32 @@ enum State {
 
 State currentState;
 
+Servo Servo1;
+Servo Servo2;
+
+
 int checkHallSensor(int sensorNum);
-void toggleSolenoid();
+void openBox();
+void closeBox();
 void flashLED10();
 
+
+
+//**********************************
+//
+//
+//**********************************
 void setup() {
   Serial.begin(9600);
   Serial.println("Geocache initializing...");
+
+  Servo1.attach(Servo1_pin);
+  Servo2.attach(Servo2_pin);
   
-  pinMode(Solenoid1, OUTPUT);
-  pinMode(Solenoid2, OUTPUT); 
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
 
-  digitalWrite(Solenoid1, LOW);
-  digitalWrite(Solenoid2, LOW);
   digitalWrite(LED1, LOW);
   digitalWrite(LED2, LOW);
   digitalWrite(LED3, LOW);
@@ -50,14 +71,26 @@ void setup() {
   currentState = start;
 
   Serial.print("Geocache opening for 10 seconds...");
-  toggleSolenoid();
+  openBox();
+  closeBox();
+  Servo1.detach();
+  Servo2.detach();
   Serial.println("LOCKED");
   Serial.println("Geocache now running...");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly
-  
+
+
+
+
+
+
+
+//**********************************
+//
+//
+//**********************************
+void loop() {  
   Serial.print("HallSensor1 Status: ");
   Status_HallSensor1 = checkHallSensor(HallSensor1);
   if (Status_HallSensor1 == 1 && currentState == start) {
@@ -75,25 +108,33 @@ void loop() {
   Serial.print("HallSensor3 Status: ");
   Status_HallSensor3 = checkHallSensor(HallSensor3);
   if (Status_HallSensor3 == 1 && currentState == letter2) {
-    currentState = opened;
     digitalWrite(LED3, HIGH);
-    toggleSolenoid();
+    openBox();
+    currentState = opened;
    }
 
   Serial.print("HallSensorLock Status: ");
   Status_HallSensorLock = checkHallSensor(HallSensorLock);
   if (Status_HallSensorLock == 1 && currentState == opened) {
+    closeBox();
     currentState = start;
-    toggleSolenoid();
    }
-
 
   Serial.print("Current State: ");
   Serial.println(currentState);
-  delay(troubleshootDelay);
+  delay(TroubleshootDelay);
 }
 
 
+
+
+
+
+
+//**********************************
+//
+//
+//**********************************
 int checkHallSensor(int sensorNum) {
   int sensorReading = analogRead(sensorNum);
   if (sensorReading < HallOnOff ) {
@@ -107,15 +148,50 @@ int checkHallSensor(int sensorNum) {
 }
 
 
-void toggleSolenoid() {
-  digitalWrite(Solenoid1, HIGH);
-  digitalWrite(Solenoid2, HIGH);
+
+
+
+//**********************************
+//
+//
+//**********************************
+void openBox() {
+  Servo1.attach(Servo1_pin);
+  Servo2.attach(Servo2_pin);
+  Servo1.write(Servo1_openPos);
+  Servo2.write(Servo2_openPos);
   flashLED10();
-  digitalWrite(Solenoid1, LOW);
-  digitalWrite(Solenoid2, LOW);
+  Servo1.detach();
+  Servo2.detach();
 }
 
 
+
+
+
+//**********************************
+//
+//
+//**********************************
+void closeBox() {
+  Servo1.attach(Servo1_pin);
+  Servo2.attach(Servo2_pin);
+  flashLED10();
+  Servo1.write(Servo1_closePos);
+  Servo2.write(Servo2_closePos);
+  Servo1.detach();
+  Servo2.detach();
+}
+
+
+
+
+
+
+//**********************************
+//
+//
+//**********************************
 void flashLED10() {
   for (int x=0; x<4; x++) {
     digitalWrite(LED1, HIGH);
